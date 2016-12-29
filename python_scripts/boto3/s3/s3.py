d93935cb0c9d7d4701b729c3cb8623c5b3867826
root <root@ip-172-31-33-145.us-west-2.compute.internal>
2016-12-29 03:27:32 +0000

    Added code for displaying the contents of bucket

--- python_scripts/boto3/s3/s3.py
+++ python_scripts/boto3/s3/s3.py
@@ -4,8 +4,16 @@ import boto3
 
 s3 = boto3.resource('s3')
 
+#print out all bucket name
+print("S3 Bucket Name:")
 for b in s3.buckets.all():
 	print(b.name)
 
+# upload a file on s3 bucket named gauravhello
 data = open('/root/hello.jpeg','rb')
 s3.Bucket('gauravhello').put_object(Key='test.jpeg',Body=data)
+
+bucket = s3.Bucket('gauravhello')
+print("S3 Bucket gauravhello file names:")
+for obj in bucket.objects.all():
+	print(obj.key)
